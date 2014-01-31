1a57086b45d6f2eb21fbae5b9111100d710a6a5c
doo803 <my_name_is_mattios@hotmail.com>
2014-02-01 02:47:04 +1300

    Finished level 12

--- Block bounce/Block bounce/levels/10-19/level12.cs
+++ Block bounce/Block bounce/levels/10-19/level12.cs
@@ -33,8 +33,130 @@ namespace Block_bounce.levels
             // Begin level design
             #region
             // Decaying platforms
-            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(100, Game1.screenHeight - 100), 
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(0, Game1.screenHeight - 70), 
                 3, 60));
+
+            // First layer 
+            #region
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(200, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(220, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(240, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(260, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(280, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(300, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(320, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(340, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(360, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(380, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(400, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(420, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(440, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(460, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(480, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(500, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(520, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(540, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(560, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(580, Game1.screenHeight - 130),
+                3, 60));
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(600, Game1.screenHeight - 130),
+                3, 60));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(700, Game1.screenHeight - 190),
+                3, 60));
+            #endregion
+
+            // Second layer 
+            #region
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(200, Game1.screenHeight - 270),
+                3, 60));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(250, Game1.screenHeight - 270)));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(300, Game1.screenHeight - 270),
+                3, 60));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(350, Game1.screenHeight - 270)));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(400, Game1.screenHeight - 270),
+                3, 60));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(450, Game1.screenHeight - 270)));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(500, Game1.screenHeight - 270),
+                3, 60));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(550, Game1.screenHeight - 270)));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(600, Game1.screenHeight - 270),
+                3, 60));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(100, Game1.screenHeight - 340),
+                3, 60));
+
+            #endregion
+
+            // Third layer
+            #region
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(200, Game1.screenHeight - 410),
+                3, 60));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(250, Game1.screenHeight - 410)));
+
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(300, Game1.screenHeight - 410)));
+
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(350, Game1.screenHeight - 410)));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(400, Game1.screenHeight - 410),
+                3, 60));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(450, Game1.screenHeight - 410)));
+
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(500, Game1.screenHeight - 410)));
+
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(550, Game1.screenHeight - 410)));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(600, Game1.screenHeight - 410),
+                3, 60));
+
+            decayingPlatformList.Add(new DecayingPlatform(Content.Load<Texture2D>("level/platform/20platform20"), new Vector2(700, Game1.screenHeight - 480),
+                3, 60));
+            #endregion
+
+            // Spikes on fall
+            #region
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(790, Game1.screenHeight - 480)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(790, Game1.screenHeight - 450)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(825, Game1.screenHeight - 450)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(790, Game1.screenHeight - 420)));
+
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(870, Game1.screenHeight - 330)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(835, Game1.screenHeight - 300)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(870, Game1.screenHeight - 300)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(870, Game1.screenHeight - 270)));
+
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(790, Game1.screenHeight - 180)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(790, Game1.screenHeight - 150)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(825, Game1.screenHeight - 150)));
+            spikeList.Add(new Spikes(Content.Load<Texture2D>("level/spike/floatingspike"), new Vector2(790, Game1.screenHeight - 120)));
+
+            #endregion
+
+            // Wall
+            platformList.Add(new Platform(Content.Load<Texture2D>("level/platform/20platform600"), new Vector2(758, 100)));
 
             // Spikes
             spikeRowList.Add(new SpikeRow(Content.Load<Texture2D>("level/spike/spike"), new Rectangle(200, Game1.screenHeight - 30, 580, 30)));
